fec6f4142c62933a52b708b0c95053aca4e4e5ff
Caskev2003 <caskev2003@gmail.com>
2025-11-26 13:18:09 -0600

    ajuste zod 5

--- src/app/(routes)/(refacciones-supervisor-rutas)/supervisor_gestion_inventarios_refacciones/components/registro-refacciones/RefaccionesForm/RefaccionesForm.form.ts
+++ src/app/(routes)/(refacciones-supervisor-rutas)/supervisor_gestion_inventarios_refacciones/components/registro-refacciones/RefaccionesForm/RefaccionesForm.form.ts
@@ -2,9 +2,9 @@ import { z } from "zod"
 
 export const refaccionSchema = z.object({
   codigo: z.number().min(1, "El código es requerido"),
-  descripcion: z.string().min(1, "La descripción es obligatoria"),
-  noParte: z.string().min(1, "El número de parte es obligatorio"),
-  proveedores: z.string().min(1, "Proveedor requerido"),
+  descripcion: z.string().min(1, "La descripción es obligatoria") .max(150, "Haz superado el limite de caracteres, el limite debe ser 80 caracteres"),
+  noParte: z.string().min(1, "El número de parte es obligatorio") .max(50, "Haz superado el limite de caracteres, el limite debe ser 80 caracteres"),
+  proveedores: z.string().min(1, "Proveedor requerido") .max(80, "Haz superado el limite de caracteres, el limite debe ser 80 caracteres"),
   fechaIngreso: z.string().min(1, "Fecha requerida"),
   unidadMedidaId: z.enum(["KG", "LTS", "PZ", "MTS"]),
   ubicacionId: z.number().min(1, "Ubicación requerida"), // Cambiado de coerce.number() a number()
