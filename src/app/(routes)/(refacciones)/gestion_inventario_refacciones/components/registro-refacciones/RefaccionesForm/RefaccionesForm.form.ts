72d49aca239f851b565648cc728c3a3017f066dc
Caskev2003 <caskev2003@gmail.com>
2025-11-26 13:17:08 -0600

    ajuste zod 4

--- src/app/(routes)/(refacciones)/gestion_inventario_refacciones/components/registro-refacciones/RefaccionesForm/RefaccionesForm.form.ts
+++ src/app/(routes)/(refacciones)/gestion_inventario_refacciones/components/registro-refacciones/RefaccionesForm/RefaccionesForm.form.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const refaccionSchema = z.object({
   codigo: z.number().min(1, "El código es requerido"),
-  descripcion: z.string().min(1, "La descripción es obligatoria") .max(80, "Haz superado el limite de caracteres, el limite debe ser 80 caracteres"),
+  descripcion: z.string().min(1, "La descripción es obligatoria") .max(150, "Haz superado el limite de caracteres, el limite debe ser 80 caracteres"),
   noParte: z.string().min(1, "El número de parte es obligatorio") .max(50, "Haz superado el limite de caracteres, el limite debe ser 80 caracteres"),
   proveedores: z.string().min(1, "Proveedor requerido") .max(80, "Haz superado el limite de caracteres, el limite debe ser 80 caracteres"),
   fechaIngreso: z.string().min(1, "Fecha requerida"),
